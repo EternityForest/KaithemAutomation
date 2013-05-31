73281cbd4b62923c8cd770b79436a29c7f193a10
Daniel Black <daniel@localhost.localdomain>
2013-05-31 00:08:06 -0700

    Added license text to every .py file, added license to help section.

--- modules.py
+++ modules.py
@@ -1,3 +1,18 @@
+#Copyright Daniel Black 2013
+#This file is part of Kaithem Automation.
+
+#Kaithem Automation is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3.
+
+#Kaithem Automation is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Kaithem Automation.  If not, see <http://www.gnu.org/licenses/>.
+
 #File for keeping track of and editing kaithem modules(not python modules)
 
 import auth,cherrypy,pages,urllib,directories,os,json,util,newevt,shutil,sys
