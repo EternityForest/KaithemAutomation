1748aec5f1e7975be343799b4777604e227608f5
Daniel Black <daniel@mint>
2013-07-15 16:00:45 -0700

    Got rid of some selenium tests, they weren't much good. Got rid of some .pyc files. Fixed bug where I forgot to sort a few menus.

---
+++ kaithem/src/tests/unit/testevents.py
@@ -0,0 +1,79 @@
+import common
+import newevt
+import time
+import messagebus
+
+#Create an event that sets y to 0 if it is 1
+x = newevt.Event("y==1","y=0",locals())
+
+#Register event with polling.
+x.register()
+#Set y to 1
+y = 1
+
+time.sleep(0.05)
+#y should immediately be set back to 0 at the next polling cycle 
+if y == 1:
+        common.fail("Edge-Triggered Event did nothing")
+        
+#Unregister event and make sure it really goes away
+x.unregister() 
+y =1
+if not y == 1:
+        common.fail("Edge-Triggered Event did not go away when unregistered")
+        
+        
+        
+        
+#Same exact thing exept we use the onchange
+x = newevt.Event("!onchange y","y=5",locals())
+
+
+#Register event with polling.
+x.register()
+#Give it a value to change from
+y = 0
+#Let it notice the old value
+time.sleep(0.10)
+#Set y to 1
+y = 1
+
+time.sleep(0.10)
+#y should immediately be set back to 0 at the next polling cycle 
+if y == 1:
+        common.fail("Onchange Event did nothing")
+        
+#Unregister event and make sure it really goes away
+x.unregister() 
+y =1
+if not y == 1:
+        common.fail("Onchange Event did not go away when unregistered")
+   
+   
+   
+        
+#Now we test the message bus event
+x = newevt.Event("!onmsg /test","y='test'",locals())
+
+
+#Register event with polling.
+x.register()
+#Give it a value to change from
+messagebus.postMessage("/test",'poo')
+#Let it notice the old value
+
+time.sleep(0.25)
+#y should immediately be set back to 0 at the next polling cycle 
+if not  y == 'test':
+        common.fail("Message Event did nothing")
+    
+#Unregister event and make sure it really goes away
+x.unregister() 
+y =1
+
+messagebus.postMessage('poo',"/test")
+if y == 'test':
+        common.fail("Message Event did not go away when unregistered")
+        
+
+print("Sucess in testing event module")
