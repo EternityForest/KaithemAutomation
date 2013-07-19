8a8c0a407460049e67e80330353501012ac14117
Daniel Black <daniel@mint>
2013-07-19 01:17:05 -0700

    Made our fake 'which' command take into account the fact that windows files end in '.exe'. also commented the function

--- kaithem/src/sound.py
+++ kaithem/src/sound.py
@@ -13,7 +13,7 @@
 #You should have received a copy of the GNU General Public License
 #along with Kaithem Automation.  If not, see <http://www.gnu.org/licenses/>.
 
-import subprocess,os,util
+import subprocess,os,util,sys
 
 #This class provides some infrastructure to play sounds but if you use it directly it is a dummy.
 class SoundWrapper(object):
