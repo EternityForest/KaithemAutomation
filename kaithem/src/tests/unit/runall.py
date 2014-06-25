d29f311f92013ec36e9bcf4659050958e4ce910a
Daniel Black <daniel@mint>
2014-06-24 18:53:36 -0700

    Version number update, test, release

--- kaithem/src/tests/unit/runall.py
+++ kaithem/src/tests/unit/runall.py
@@ -1,5 +1,13 @@
 import sys,os
 sys.path.append(os.path.join(sys.path[0],'..','..','thirdparty'))
+
+if sys.version_info < (3,0):
+    sys.path.append(os.path.join(sys.path[0],'..','..','thirdparty',"python2"))
+    from gzip import open as opengzip
+else:
+    from gzip import GzipFile as opengzip
+    sys.path.append(os.path.join(sys.path[0],'..','..','thirdparty',"python3"))
+    
 import TestMessageBus
 import testevents
 import testunitconversions
