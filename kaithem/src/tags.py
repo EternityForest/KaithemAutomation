b72fb168c3042142b98b7c55efffcc59cbee3051
Daniel Dunn <eternityforest@gmail.com>
2015-05-21 09:36:20 -0700

    tags

--- kaithem/src/tags.py
+++ kaithem/src/tags.py
@@ -1,22 +1,25 @@
 import weakref,traceback, time
 #from src import util
 
-A tag point is very similar to a variable, except for the fact that you can subscribe to it and be notified if it changes py passing a function that takes
-one argument to tag.subscribe. You must maintain a reference to f or it will be garbage collected.
-The subscribe function returns a value that may be passed to unsubscribe for a more reliable means of deletion,
+# A tag point is very similar to a variable, except for the fact that you can subscribe to it and be notified if it changes py passing a function that takes
+# one argument to tag.subscribe. You must maintain a reference to f or it will be garbage collected.
+#
+# The subscribe function returns a value that may be passed to unsubscribe for a more reliable means of deletion,
+#
+# To read the current value of a tag point, simply call it. The value will be returned, and if there are any subscribers, it will be passed to them as well.
+#
+# To write to the tag point, simply call it with one numeric argument.
+#
+# If you have set up a getter, the getter will be called whenever the tag points value is requested, and the value it returns will be used.
+#
+# tag.interval determines how long tag values can be cached for in seconds. tag.age is a property(not a function) that gives the total time since last updated.
+#
+# tag.require tells the tag point that a permission is needed to read to write.
+#
+# tag.meter takes the same parameters as widget.Meter and returns a meter that shows the value of the tag point.
+# Its permissions will be exactly the same as the point.
 
-To read the current value of a tag point, simply call it. The value will be returned, and if there are any subscribers, it will be passed to them as well.
-
-To write to the tag point, simply call it with one numeric argument.
-
-If you have set up a getter, the getter will be called whenever the tag points value is requested, and the value it returns will be used.
-
-tag.interval determines how long tag values can be cached for in seconds. tag.age is a property(not a function) that gives the total time since last updated.
-
-tag.require tells the tag point that a permission is needed to read to write.
-
-tag.meter takes the same parameters as widget.Meter and returns a meter that shows the value of the tag point.
-Its permissions will be exactly the same as the point.
+#Tag
 
 class Tag():
     def __init__(self,name="Untitled_Tag", getter=None, default=0):
@@ -36,6 +39,15 @@ class Tag():
     #then modifies things to pass through reads and writes. Actually checking permissions is handled by widget.py.
     def meter(self,*args,**kwargs):
         m = widgets.Meter(*args.**kwargs)
+        m._write_perms = self.write_permissions
+        m._read_perms = self.read_permissions
+        def f(obj, usr):
+            return self()
+        m.onRequest = f
+        return m
+
+    def slider(self,*args,**kwargs):
+        m = widgets.Slider(*args.**kwargs)
         m._write_perms = self.write_permissions
         m._read_perms = self.read_permissions
         def f(obj, usr):
@@ -103,3 +115,37 @@ class Tag():
 
     def requireToWrite(self,p):
         self.write_permissions.append(p)
+
+class CVFilter(Tag):
+        def __init__(self,name="Untitled_Tag", getter=None, default=0):
+            self.target = 0;
+            Tag.__init__(self,name,getter,default)
+
+        def set(self,value):
+            self.value = value
+            self.target = value
+            self.updated = time.time()
+
+        def write(self.value):
+            change = self.rate*self.age()
+            self.value = min(self.value-chane, max(self.value+change,self.target))
+            self.target = value
+            self.updated = time.tim
+            self,_push(value)
+
+            def __call__(self,*args):
+                change = self.rate*self.age()
+                self.value = min(self.value-chane, max(self.value+change,self.target))
+                if args:
+                    self.write(args[0])
+                    self.updated = time.time
+                    return
+                if self.getter() and self.age>self.interval:
+                    try:
+                        self.target = self.getter()
+                        self.updated = time.time
+                    except Exception as e:
+                        messagebus.postMessage("system/tagpoints/errors", traceback.format_tb(6))
+                    self._push(self.value)
+                else:
+                    return self.value
