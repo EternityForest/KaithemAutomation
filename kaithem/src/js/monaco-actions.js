97a8389bff16ed8710afda81215de60902d76623
Daniel Dunn <eternityforest@gmail.com>
2017-07-16 19:43:12 -0700

    Prettify

--- kaithem/src/js/monaco-actions.js
+++ kaithem/src/js/monaco-actions.js
@@ -8,6 +8,53 @@ var insert =function(editor,text)
     }
 
 
+
+function escws(s)
+        {   
+            //all runs of whitespace get paren-ified
+            var s=s.replace(/[\t ]*/,"($1)")
+
+            //Double all real backticks, we will use single backticks as our
+            //escape symbol for a newline
+            var s=s.replace("`","``")
+            var s=s.replace("\n","`n")
+            var s=s.replace("\r","`r")
+            return "<mako-literal>"+s+"</mako-literal>"
+        }
+
+
+function unescws(s)
+        {   
+            return s
+
+            var s=s.replace(/\([\t ]\)*/,"$1")
+            var s=s.replace("`n","\n")
+            var s=s.replace("`r","\r")
+            //Remaining double backticks are real
+            var s=s.replace("``","`")
+        }
+
+function add_mako_format(e)
+{
+    e.addAction({
+	id: 'format-id',
+	label: 'Format',
+	contextMenuGroupId: 'edit',
+	contextMenuOrder: 1.9,
+
+	run: function(ed) {
+		var x = ed.getValue();
+        x = x.replace(/\n(\%[\t ]*.*)\n/g,"<mako-literal>$1</mako-literal>")
+        x = tidy_html5(x,{"indent":true,"show-body-only":true,"wrap-asp":false, "new-inline-tags":"mako-literal"})
+        x = x.replace(/(.)\<mako\-literal\>/g,"$1\n<mako-literal>")
+        x = x.replace(/\<mako\-literal\>(.*?)\<\/mako-literal\>/g,"$1\n")
+        ed.setValue(x)
+		return null;
+	}
+});
+
+
+}
 function add_actions(e)
 {
     e.addAction({
