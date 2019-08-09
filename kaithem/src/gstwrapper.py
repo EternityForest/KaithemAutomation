f231537299580f8f69c1dc3da4585400800526a2
Daniel Dunn <dannydunn@eternityforest.com>
2019-08-08 18:06:30 -0700

    Pretty good progress on the mixer

---
+++ kaithem/src/gstwrapper.py
@@ -0,0 +1,161 @@
+
+#Copyright Daniel Dunn 2019
+#This file is part of Kaithem Automation.
+
+#Kaithem Automation is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3.
+
+#Kaithem Automation is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+
+
+#This file really shouldn't have too many non-essential dependancies onthe rest of kaithem,
+#Aside from the threadpool and the message bus.
+
+import threading
+
+from . import jackmanager
+
+initialized = False
+initlock = threading.Lock()
+Gst = None
+
+def init():
+    "On demand loading, for startup speed but also for compatibility if Gstreamer isn't there"
+    global initialized, Gst
+    with initlock:
+        if not initialized:
+            import gi
+            gi.require_version('Gst', '1.0')
+            gi.require_version('GstBase', '1.0')
+            gi.require_version('Gtk', '3.0')
+            from gi.repository import Gst as gst
+            from gi.repository import GObject, GstBase, Gtk, GObject
+            Gst = gst
+            Gst.init(None)
+            initialized = True
+
+class Pipeline():
+    def __init__(self, name, channels= 2, input=None, outputs=[]):
+        init()
+        self.lock = threading.Lock()
+        self.pipeline = Gst.Pipeline()
+        self.name = name
+
+        self.src = Gst.ElementFactory.make('jackaudiosrc')
+        self.capsfilter = Gst.ElementFactory.make('capsfilter')
+
+        self.capsfilter.caps = Gst.Caps("audio/x-raw, channels="+str(channels))
+
+        self.sink = Gst.ElementFactory.make('jackaudiosink')
+       
+
+
+        self.src.connect = 0
+        self.sink.connect=0
+
+        #Random nonsense so they don't auto connect
+        self.src.set_property("port-pattern","fdgjkndgmkndfmfgkjkf")
+        self.sink.set_property("port-pattern","fdgjkndgmkndfmfgkjkf")
+
+        self.src.set_property("client-name",name+"_in")
+        self.sink.set_property("client-name",name+"_out")
+
+
+        self.pipeline.add(self.src)
+        self.pipeline.add(self.capsfilter)
+        self.pipeline.add(self.sink)
+        self.channels = channels
+
+        if not self.src.link(self.capsfilter):
+            raise RuntimeError
+
+        self.elements = [self.capsfilter]
+        self.input=input
+        self._input= None
+        self.outputs=outputs
+
+        self.sends = []
+        self.sendAirwires =[]
+
+    def connect(self):
+        self._outputs = []
+        for i in self.outputs:
+            x = jackmanager.Airwire(self.name+"_out", i)
+            x.connect()
+            self._outputs.append(x)
+
+        self._input = jackmanager.Airwire(self.input, self.name+"_in") 
+        self._input.connect()
+
+    def finalize(self):
+        if not self.elements[-1].link(self.sink):
+            raise RuntimeError("Could not link to jack sink")
+        self.pipeline.set_state(Gst.State.PLAYING)
+
+    def stop(self):
+        with self.lock:
+            for i in self.sendAirwires:
+                i.disconnect()
+            if self._input:
+                self._input.disconnect()
+            for i in self._outputs:
+                i.disconnect()
+        
+            self.pipeline.set_state(Gst.State.NULL)
+
+    def addElement(self,t,name=None):
+        if not isinstance(t, str):
+            raise ValueError("Element type must be string")
+
+        e = Gst.ElementFactory.make(t,name)
+
+        if e==None:
+            raise ValueError("Nonexistant element type")
+
+        self.pipeline.add(e)
+        self.elements[-1].link(e)
+        self.elements.append(e)
+        return e
+
+    def addSend(self,target):
+        if not isinstance(target, str):
+            raise ValueError("Targt must be string")
+
+        e = Gst.ElementFactory.make("tee")
+
+        if e==None:
+            raise ValueError("Nonexistant element type")
+
+        e2 = Gst.ElementFactory.make("jacksink")
+
+        if e2==None:
+            raise ValueError("Nonexistant element type jacksink")
+
+        self.pipeline.add(e)
+        self.pipeline.add(e2)
+        e2.set_property("port-pattern","fdgjkndgmkndfmfgkjkf")
+        #Sequentially number the sends
+        cname = self.name+"_send"+str(len(self.sends))
+        e2.set_property("client-name",cname)
+        self.sendAirwires.append(jackmanager.Airwire(cname, target))
+        self.sends.append(e2)
+
+
+        self.elements[-1].link(e)
+        self.elements.append(e)
+        return e
+
+# import time
+# p = Pipeline("test", outputs=["system"])
+# p.finalize()
+# time.sleep(2)
+# p.connect()
+
+# while(1):
+#     pass
---
+++ kaithem/src/jackmixer.py
@@ -0,0 +1,158 @@
+#Copyright Daniel Dunn 2013,2017
+#This file is part of Kaithem Automation.
+
+#Kaithem Automation is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, version 3.
+
+#Kaithem Automation is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Kaithem Automation.  If not, see <http://www.gnu.org/licenses/>.
+
+import re, jack,time
+
+from . import widgets, messagebus
+from . import jackmanager, gstwrapper
+
+global_api =widgets.APIWidget()
+global_api.require("/users/mixer.edit")
+
+#Configured list of mixer channel strips
+channels = {}
+
+def replaceClientNameForDisplay(i):
+    x = i.split(':')[0]
+    if x in jackmanager.portJackNames:
+        return i.replace(x,jackmanager.portJackNames[x])
+    
+    return i
+
+def onPortAdd(t,m):
+    #m[1] is true of input
+    global_api.send(['newport', m[0],m[1]])  
+def onPortRemove(t,m):
+    #m[1] is true of input
+    global_api.send(['rmport', m[0]])
+
+messagebus.subscribe("/system/jack/newport/", onPortAdd)
+messagebus.subscribe("/system/jack/rmport/", onPortRemove)
+
+
+effectTemplates={
+    "fader":{"type":"fader", "displayType": "Fader", "help": "The main fader for the channel",
+    "params": []
+    },
+    "3beq":{"type":"fader", "displayType":"3 Band EQ","help": "Basic builtin EQ",
+        "params": {
+            "high": {
+                "type":"float",
+                "displayName": "High",
+                "value": 0,
+                "min": -12,
+                "max": 12,
+                "sort":0
+            }
+        }
+    }
+}
+
+class MixingBoard():
+    def __init__(self, *args, **kwargs):
+        self.api =widgets.APIWidget()
+        self.api.require("/users/mixer.edit")
+        self.api.attach(self.f)
+        self.channels = {}
+        self.channelObjects ={}
+
+
+    def sendPorts(self):
+        inPorts = jackmanager.getPorts(is_audio=True, is_output=False)
+        outPorts = jackmanager.getPorts(is_audio=True, is_output=True)
+        self.api.send(['inports',sorted([replaceClientNameForDisplay(i.name) for i in  inPorts]) ])
+        self.api.send(['outports',sorted([replaceClientNameForDisplay(i.name) for i in  outPorts]) ])
+        self.api.send(['channels', channels])
+
+    def createChannel(self, name,data):
+        # import time
+        op = data['output'].split(",")
+
+
+        if name in self.channelObjects:
+            self.channelObjects[name].stop()
+        time.sleep(0.01)
+        time.sleep(0.01)
+        time.sleep(0.01)
+        time.sleep(0.01)
+        time.sleep(0.01)
+
+        p = gstwrapper.Pipeline(name, outputs=op, input=data['input'])
+        p.effectsList = []
+        p.fader=None
+
+        for i in data['effects']:
+            if i['type']=="fader":
+                p.fader= p.addElement("volume")
+        p.finalize()
+        p.connect()
+        self.channelObjects[name]=p
+
+    def deleteChannel(self,name):
+        if name in self.channels:
+            del self.channels[name]
+        if name in self.channelObjects:
+            self.channelObjects[name].stop()
+            del self.channelObjects[name]
+
+
+
+    def f(self,user, data):
+        if data[0]== 'refresh':
+            self.sendPorts()
+        if data[0]=='addChannel':
+            #No overwrite
+            if data[1] in self.channels:
+                return
+            #No empty names
+            if not data[1]:
+                return
+            self.channels[data[1]] = {"effects":[effectTemplates['fader']], "input": '', 'output': '', "fader":-60}
+            self.api.send(['channels', self.channels])
+            self.createChannel(data[1], self.channels[data[1]])
+
+        if data[0]=='setEffects':
+            "Directly set the effects data of a channel"
+            self.channels[data[1]]['effects']= data[2]
+            self.api.send(['channels', self.channels])
+            self.createChannel(data[1], self.channels[data[1]])
+
+
+        if data[0]=='setFader':
+            "Directly set the effects data of a channel"
+            self.channels[data[1]]['fader']= float(data[2])
+            self.api.send(['fader', data[1], data[2]])
+            if data[2]>-80:
+                self.channelObjects[data[1]].fader.set_property('volume', 10**(float(data[2])/20))
+            else:
+                self.channelObjects[data[1]].fader.set_property('volume', 0)
+
+        if data[0]=='setParam':
+            "Directly set the effects data of a channel. Params and effects don't have names"
+            self.channels[data[1]]['effects'][data[2]]['params'][data[3]]['value'] = data[4]
+            self.api.send(['param', data[1],data[2], data[3], data[4]])
+
+
+        if data[0]=='addEffect':
+            self.channels[data[1]]['effects'].append(effectTemplates[data[2]])
+            self.api.send(['channels', self.channels])
+            self.createChannel(data[1], self.channels[data[1]])
+
+        if data[0]=='rmChannel':
+            del self.channels[data[1]]
+            self.api.send(['channels', self.channels])
+            self.deleteChannel(data[1])
+
+board = MixingBoard()
