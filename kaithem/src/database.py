e3e4ae9fb14d0c4366614ddd5e3c6347b3875235
Daniel Dunn <dannydunn@eternityforest.com>
2020-10-28 21:26:25 -0700

    DrayerDB is slowly moving towards a usable state

--- kaithem/src/database.py
+++ kaithem/src/database.py
@@ -4,49 +4,6 @@
 
 # There is one table called Document
 
-# uuid PRIMARY KEY:  UUID of the document in canonical text form
-# name: Renamable name for the document
-# type: The type of this document. Type names not starting with app. are reserved
-# parent: The UUID of the parent document, used for links and properties. Can be empty.
-# link: The UUID of another document, used for graph links. Can be empty.
-# timestamp: Last time the record was changed
-# local_timestamp: When this node's copy of the document changed(For remote sync)
-# document: A JSON document. Keys starting with "." are reserved. Always an object.
-# blob: generic blob storage for embedded files.
-# signature: Either empty, or reserved for digitally signing the message.
-
-
-# The document must always be a dict, and keys other than app. are reserved.
-
-# Every value must be a 2-element list called a cell, where the first item is data
-# and the second item is the cell properties.
-
-
-# The cell properties field may contain a "type" key which may be "table",
-# in which case there will be a "query" item.
-
-# Having a table as a cell means that viewer pages should embed a table
-# of documents which is to be created from the given query.
-#
-# The query will be a genuine SQL statement, using AS to name the columns.
-
-
-# Any type beginning with . is reserved.
-
-# .template types have names that match the types of other records. The other records inherit all unset properties.
-# .prop types have a parent that matches a UUID of another record. They override any keys that are set in their document.
-# Only one .prop may exist per name for any given record, adding a new with the saem name deletes the old.
-
-# Note that you can't update just one key of a document without replacing it totally, prop overrides
-# are used to let you set different props of the same node from different servers without conflict.
-
-# .link types are used to link documents, the name is the type of relation, but they are otherwise normal documents.
-# .elink is the same, but they are exclusive, adding one deletes the old one of the same name.
-
-# .log types are used to associate a log entry with a specific node. They have no special meaning, but they an be truly deleted instead of set to NULL
-# after a certain time without manual intervention
-# and as such there is no guarantee they stay deleted, but they usually will.
-
 import sqlite3
 import time
 import json
@@ -57,8 +14,9 @@ import os
 import libnacl
 import base64
 import struct
+import uuid
 
-from scullery import messagebus
+# from scullery import messagebus
 
 # class DocumentView():
 #     def __init__(self,database,uuid):
@@ -82,10 +40,15 @@ from scullery import messagebus
 #             if key in x[0]:
 #                 return x[0][key]
 
+
+def jsonEncode(d):
+    return json.dumps(d, sort_keys=True, indent=4, separators=(',', ': '))
+
+
 class DocumentDatabase():
     def __init__(self, filename, publicKey=None):
 
-        self.mbname = "/drayerDB/db/"+os.path.abspath(filename)
+        self.filename = os.path.abspath(filename)
 
         self.conn = sqlite3.connect(filename)
         self.config = configparser.ConfigParser()
@@ -94,33 +57,64 @@ class DocumentDatabase():
             self.config.read(filename+".conf")
 
         self.conn.row_factory = sqlite3.Row
-        #self.conn.execute("PRAGMA wal_checkpoint=FULL")
-        self.conn.execute("PRAGMA schema.secure_delete = off")
+        # Self.conn.execute("PRAGMA wal_checkpoint=FULL")
+        self.conn.execute("PRAGMA secure_delete = off")
 
-        self.conn.execute('''CREATE TABLE IF NOT EXISTS document
-             (_uuid text PRIMARY KEY,_time integer, _arrival integer, _parent text, _link text, _type text, _name text, 
-              document text, data blob, signature blob)''')
+        # Yep, we're really just gonna use it as a document store like this.
+        self.conn.execute(
+            '''CREATE TABLE IF NOT EXISTS document (rowid integer primary key, json text, signature text, localinfo text)''')
 
         self.conn.execute('''CREATE TABLE IF NOT EXISTS meta
-             (key text, value  text)''')
+             (key text primary key, value  text)''')
+
+        # To keep indexing simple and universal, it only works on three properties.  _tags, _description and _body.
+        self.conn.execute('''
+             CREATE VIRTUAL TABLE IF NOT EXISTS search USING fts5(tags, description, body, content='')''')
 
         self.conn.execute(
-            '''CREATE INDEX IF NOT EXISTS document_parent ON document(_parent) WHERE _parent IS NOT "" ''')
+            '''CREATE INDEX IF NOT EXISTS document_parent ON document(json_extract(json,"$._parent")) WHERE json_extract(json,"$._parent") IS NOT null ''')
         self.conn.execute(
-            '''CREATE INDEX IF NOT EXISTS document_link ON document(_link) WHERE _link IS NOT "" ''')
+            '''CREATE INDEX IF NOT EXISTS document_link ON document(json_extract(json,"$._link")) WHERE json_extract(json,"$._link") IS NOT null''')
         self.conn.execute(
-            '''CREATE INDEX IF NOT EXISTS document_name ON document(_name)''')
+            '''CREATE INDEX IF NOT EXISTS document_name ON document(json_extract(json,"$._name"))''')
         self.conn.execute(
-            '''CREATE INDEX IF NOT EXISTS document_timestamp ON document(_uuid)''')
+            '''CREATE INDEX IF NOT EXISTS document_id ON document(json_extract(json,"$._id"))''')
         self.conn.execute(
-            '''CREATE INDEX IF NOT EXISTS document_type ON document(_type)''')
+            '''CREATE INDEX IF NOT EXISTS document_type ON document(json_extract(json,"$._type"))''')
+
+        self.conn.execute(
+            """
+            CREATE TRIGGER IF NOT EXISTS search_index AFTER INSERT ON document BEGIN
+            INSERT INTO search(rowid, tags, description, body) VALUES (new.rowid, IFNULL(json_extract(new.json,"$._tags"), ""), IFNULL(json_extract(new.json,"$._description"), ""), IFNULL(json_extract(new.json,"$._body"), ""));
+            END;
+            """)
+
+        self.conn.execute(
+            """   CREATE TRIGGER IF NOT EXISTS search_delete AFTER DELETE ON document BEGIN
+            INSERT INTO search(search, rowid, tags, description, body) VALUES ('delete', old.rowid, IFNULL(json_extract(old.json,"$._tags"), ""), IFNULL(json_extract(old.json,"$._description"), ""), IFNULL(json_extract(old.json,"$._body"), ""));
+            END;""")
+
+        self.conn.execute(
+            """
+            CREATE TRIGGER IF NOT EXISTS search_update AFTER UPDATE ON document BEGIN
+            INSERT INTO search(search, rowid, tags, description, body) VALUES ('delete', old.rowid, IFNULL(json_extract(old.json,"$._tags"), ""), IFNULL(json_extract(old.json,"$._description"), ""), IFNULL(json_extract(old.json,"$._body"), ""));
+            INSERT INTO search(rowid, tags, description, body) VALUES (new.rowid, IFNULL(json_extract(new.json,"$._tags"), ""), IFNULL(json_extract(new.json,"$._description"), ""), IFNULL(json_extract(new.json,"$._body"), ""));
+            END;
+            """
+        )
 
         self.keys = configparser.ConfigParser()
-        secretKey=None
+        secretKey = None
 
         if os.path.exists(filename+".keys"):
             self.keys.read(filename+".keys")
 
+        try:
+            publicKey = base64.b64decode(publicKey)
+        except:
+            pass
+
+        #Have a pubkey directly specified, ensure match
         if publicKey:
             if self.keys.get('key', 'public', fallback=None):
                 pk = base64.b64decode(self.keys.get('key', 'public'))
@@ -134,32 +128,39 @@ class DocumentDatabase():
             if not publicKey:
                 publicKey = self.getMeta("publicKey")
             if not publicKey:
-                publicKey, secretKey = libnacl.crypto_keypair()
+                publicKey, secretKey = libnacl.crypto_sign_keypair()
+            else:
+                publicKey = base64.b64decode(publicKey)
 
         if not self.keys.get('key', 'public', fallback=None):
             try:
-                self.keys.addSection('key')
+                self.keys.add_section('key')
             except:
                 pass
             self.keys.set('key', 'public',
                           base64.b64encode(publicKey).decode())
             self.saveConfig()
 
+        sk =  self.keys.get('key', 'private', fallback=None)
+        if sk:
+            secretKey = secretKey or base64.b64decode(sk)
+
         if secretKey and not self.keys.get('key', 'private', fallback=None):
             try:
-                self.keys.addSection('key')
+                self.keys.add_section('key')
             except:
                 pass
+
             self.keys.set('key', 'private',
                           base64.b64encode(secretKey).decode())
             self.saveConfig()
 
         if not self.getMeta('publicKey'):
-            self.setMeta("publicKey", base64.b64encode(pk).decode())
+            self.setMeta("publicKey", base64.b64encode(publicKey).decode())
 
-        
         self.publicKey = publicKey
         self.secretKey = secretKey
+
 
     def getMeta(self, key):
         cur = self.conn.cursor()
@@ -186,11 +187,6 @@ class DocumentDatabase():
         with open(self.filename+".keys", 'w') as configfile:
             self.keys.write(configfile)
 
-
-    def updateDocument(self, uuid, type, name, document, parent, link, ts_int, blob, localts_int):
-        self.conn.execute("UPDATE document  SET _parent=?, _link=?, _type=?, _name=?, _time=?, _arrival=?, document=?, data=?, signature=? WHERE _uuid=?",
-                          (parent, link, type, name, ts_int, localts_int, json.dumps(document), blob, b'', uuid))
-
     def __enter__(self):
         self.conn.__enter__
         return self
@@ -198,86 +194,68 @@ class DocumentDatabase():
     def __exit__(self, *a):
         self.conn.__exit__
 
-    def recordToSigningBytes(self, type, name, document, parent='', link='', uuid=None, timestamp=None, arrival=None, blob=b''):
-        # Hash everything, put it together, then sign it.
-        return (libnacl.crypto_generichash(uuid)+libnacl.crypto_generichash(type)+libnacl.crypto_generichash(name) +
-                struct.pack("<Q", timestamp)+struct.pack("<Q", arrival) + libnacl.crypto_generichash(document)+libnacl.crypto_generichash(blob))
-
-    def deleteDocument(self, uuid):
         ts = int((time.time())*10**6)
 
-        # Clear everything, but keep the parent field, we can use that to let us fully GC it when the parent is deleted.
-        self.conn.execute("UPDATE document  SET link=?, type=?, name=?,timestamp=?, local_timestamp=?, document=?, data=?, signature=? WHERE uuid=?",
-                          ("", ".null", "", ts, ts, "{}", b'', b'', uuid))
-        self.conn.execute("DELETE FROM document WHERE parent=?", (uuid,))
-
-    def insertDocument(self, type, name, document, parent='', link='', uuid=None, timestamp=None, blob=b'', signature=b''):
+    def setDocument(self, doc, signature=b''):
         if not signature and not self.secretKey:
-            raise ValueError("Cannot sign any new documents, you do not have the keys")
-        ts = int((timestamp or time.time())*10**6)
-        localts = int((timestamp or time.time())*10**6)
+            raise ValueError(
+                "Cannot sign any new documents, you do not have the keys")
+
+        doc['_time'] = doc.get('_time', time.time()*1000000)
+        doc['_arrival'] = doc.get('_arrival', time.time()*1000000)
+        doc['_id'] = doc.get('_id', str(uuid.uuid4()))
+        doc['_name'] = doc.get('_name', doc['_id'])
+        doc['_type'] = doc.get('_type', '')
 
         # If a UUID has been supplied, we want to erase any old record bearing that name.
-        if uuid:
-            cur = self.conn.cursor()
-            cur.execute(
-                "SELECT _time FROM document WHERE _uuid=?", (uuid,))
-            x = cur.fetchone()
+        cur = self.conn.cursor()
+        cur.execute(
+            'SELECT json_extract(json,"$._time") FROM document WHERE  json_extract(json,"$._id")=?', (doc['_id'],))
+        x = cur.fetchone()
 
-            if x:
-                # Check that record we are trying to insert is newer, else ignore
-                if x[0] < ts:
-                    self.updateDocument(uuid, type=type, name=name, document=document,
-                                        parent=parent,  link=link, ts_int=ts, localts_int=localts, blob=blob)
+        if x:
+            # Check that record we are trying to insert is newer, else ignore
+            if x[0] < ts:
+                self.conn.execute("UPDATE document SET json=?, signature=? WHERE json_extract(json,'$._id')=?", (jsonEncode(
+                    doc), signature,  doc['_id']))
 
-                    # If we are marking this as deleted, we can ditch everything that depends on it.
-                    # We don't even have to just set them as deleted, we can relly delete them, the deleted parent record
-                    # is enough for other nodes to know this shouldn't exist anymore.
-                    if type == ".null":
-                        self.conn.execute(
-                            "DELETE FROM document WHERE _parent=?", (uuid,))
+                # If we are marking this as deleted, we can ditch everything that depends on it.
+                # We don't even have to just set them as deleted, we can relly delete them, the deleted parent record
+                # is enough for other nodes to know this shouldn't exist anymore.
+                if doc['_type'] == "_null":
+                    self.conn.execute(
+                        "DELETE FROM document WHERE json_extract(json,'$._id')=?", (doc['_id'],))
 
-                    return uuid
-                else:
-                    return uuid
+                return doc['_id']
+            else:
+                return doc['_id']
 
-        if type in (".prop", ".elink"):
-            cur = self.conn.cursor()
-            cur.execute(
-                "SELECT _time,_uuid FROM document WHERE _parent=? AND _name=? AND _type=?", (parent, name, type))
-            x = cur.fetchone()
-            if x:
-                # If a uuid was explicitly specified we can't update some other record, we have
-                # To make the DB state include exactly the record they gave us so nodes can sync.
-                if x[0] > ts and (not uuid or uuid == x[1]):
-                    # Now we have the UUID, just update that one
-                    self.updateDocument(x[1], type=type, name=name, document=document,
-                                        parent=parent,  link=link, ts_int=ts, localts_int=localts, blob=blob)
-                    return x[1]
-                else:
-                    # If we have to just insert a new record then we just clear out the old records, they have been obsoleted.
-                    cur.execute(
-                        "DELETE FROM document WHERE _parent=? AND _name=? AND _type=?", (parent, name, type))
+        # If we are generating a new message, sign it automatically.
+        if not signature:
+            print(self.secretKey)
+            signature = libnacl.crypto_sign(jsonEncode(doc).encode('utf8'), self.secretKey)
 
-        uuid = uuid or str(uuidModule.uuid4())
+        self.conn.execute(
+            "INSERT INTO document VALUES (null,?,?,?)", (jsonEncode(doc), signature,''))
 
-        self.conn.execute("INSERT INTO document VALUES (?,?,?,?,?,?,?,?,?,?)",
-                          (uuid, ts, localts, parent, link, type, name,  json.dumps(document), blob, b''))
-        return uuid
+        return doc['_id']
 
 
 d = DocumentDatabase("test.db")
 
 with d:
     for i in range(1):
-        id = d.insertDocument("Butt", "buttname", {"v": random.random()})
-        d.insertDocument(".prop", "fartsmell", {'smell': 'farty'}, parent=id)
-        d.insertDocument(".prop", "poosmell", {'poosmell': 'nasty'}, parent=id)
-        d.insertDocument(".prop", "fartsmell", {'smell': 'farty2'}, parent=id)
-        torm = d.insertDocument(".prop", "fartsmell2", {
-                                'att': 'deletethis'}, parent=id)
 
-        d.deleteDocument(torm)
-        print(d.getMergedDocument(id))
+        #Parent document
+        id = d.setDocument({
+                'someUserData': 9908            
+            })
+        
+        #Child document
+        d.setDocument({
+            '_parent': id
+        })
+    
+
 
 d.conn.commit()
