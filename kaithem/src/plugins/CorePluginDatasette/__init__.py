0a98775a5fd06b5b22bf2703b82c0ddc584d373a
Daniel Dunn <dannydunn@example.com>
2024-05-15 15:32:42 -0600

    Adds datasette

---
+++ kaithem/src/plugins/CorePluginDatasette/__init__.py
@@ -0,0 +1,27 @@
+import getpass
+import os
+import socket
+
+from datasette.app import Datasette
+
+from kaithem.api import web as webapi
+from kaithem.src import directories
+
+dbs = []
+
+logdir = directories.logdir
+
+historyFilemame = socket.gethostname() + "-" + getpass.getuser() + "-taghistory.sqlite"
+
+newHistoryDBFile = os.path.join(logdir, historyFilemame)
+
+if os.path.exists(newHistoryDBFile):
+    dbs.append(newHistoryDBFile)
+
+datasette_application = Datasette(
+    dbs,
+    settings={
+        "base_url": "/datasette/",
+    },
+).app()
+webapi.add_asgi_app("/datasette/?(/.*)?", datasette_application)
