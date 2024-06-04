3343696160506f6329d233eaeac1ddbd9c4782a9
Daniel Dunn <dannydunn@example.com>
2024-06-04 12:07:54 -0600

    Test pages and events passing

--- playwright/test-pages_events.spec.ts
+++ playwright/test-pages_events.spec.ts
@@ -9,11 +9,15 @@ test('test', async ({ page }) => {
     */
     test.setTimeout(2400000);
     await login(page);
+
+    // Create a module
     await page.getByRole('link', { name: 'Modules' }).click();
     await page.getByRole('link', { name: 'Add' }).click();
     await page.getByLabel('Name of New Module').click();
     await page.getByLabel('Name of New Module').fill('PlaywrightBasicModuleFeatures');
     await page.getByRole('button', { name: 'Submit' }).click();
+
+    // Create a page
     await page.getByRole('link', { name: 'Page' }).click();
     await page.getByLabel('Name').click();
     await page.getByLabel('Name').fill('p1');
@@ -23,11 +27,19 @@ test('test', async ({ page }) => {
     await page.locator('div').filter({ hasText: '{% extends "pagetemplate.j2.' }).nth(2).click();
     await page.locator('div').filter({ hasText: '{% extends "pagetemplate.j2.' }).nth(2).fill('    {{foo}}{{setup}}567\n\n');
     await page.getByRole('button', { name: 'Save and go to page' }).click();
-    await expect(page.getByText('p1 Content here')).toContainText('123234567');
+    
+    
+    await expect(page.getByText('Content here')).toContainText('123234567');
     await expect(page.getByRole('heading')).toContainText('p1');
+
+
     await page.getByRole('link', { name: 'Modules' }).click();
     await page.getByRole('link', { name: 'PlaywrightBasicModuleFeatures' }).click();
-    await expect(page.locator('h3')).toContainText('p1 (page)');
+
+    // Make sure it's in the list
+    await expect(page.getByRole('link', { name: 'p1 (page)' })).toContainText('p1 (page)');
+
+    // Create an event
     await page.getByRole('link', { name: 'Event' }).click();
     await page.getByLabel('Name').click();
     await page.getByLabel('Name').fill('e1');
@@ -36,28 +48,36 @@ test('test', async ({ page }) => {
 
     await page.locator('#triggerbox').fill('True');
     await page.getByRole('button', { name: 'Save Changes' }).click();
-    await expect(page.locator('dl')).toContainText('ago');
+    await expect(page.getByText('Trigger: True Last Ran:')).toContainText('ago');
     await page.getByRole('link', { name: 'e1 (event)' }).click();
+
+    // Check for the "this last ran X seconds ago"
     await expect(page.getByRole('paragraph')).toContainText('ago');
     await page.goto("http://localhost:8002/modules/module/PlaywrightBasicModuleFeatures#resources");
 
+    // Rename the page
     await page.getByRole('link', { name: 'Move' }).nth(1).click();
     await page.getByLabel('New Name').click();
     await page.getByLabel('New Name').fill('p1_new');
     await page.getByRole('button', { name: 'Submit' }).click();
     await page.getByRole('link', { name: 'Go to page' }).click();
     await page.locator('html').click();
-    await expect(page.getByText('p1 Content here')).toContainText('123234567');
+    await expect(page.getByText('Content here')).toContainText('123234567');
     await page.goto("http://localhost:8002/modules/module/PlaywrightBasicModuleFeatures#resources");
 
+    //Check page works at new location
     await page.getByRole('link', { name: 'p1_new (page)' }).click();
     await expect(page.getByRole('heading')).toContainText('PlaywrightBasicModuleFeatures: p1_new');
     await page.goto("http://localhost:8002/modules/module/PlaywrightBasicModuleFeatures#resources");
+
+    // Move the event
     await page.getByRole('link', { name: 'Move' }).first().click();
     await page.getByLabel('New Name').click();
     await page.getByLabel('New Name').fill('e1_new');
     await page.getByRole('button', { name: 'Submit' }).click();
-    await expect(page.locator('dl')).toContainText('ago');
+    await expect(page.getByText('Trigger: True Last Ran:')).toContainText('ago');
+
+
     await page.getByRole('link', { name: 'e1_new (event)' }).click();
     await expect(page.locator('h2')).toContainText('e1_new');
     await page.getByRole('link', { name: 'Modules' }).click();
