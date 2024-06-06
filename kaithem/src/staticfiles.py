dffca67e7f8ee8512df3c8edcd53cc33734f04a6
Daniel Dunn <dannydunn@example.com>
2024-06-05 19:14:56 -0600

    Just do the staticfiles in quart for now, it had some timeout issue before

--- kaithem/src/staticfiles.py
+++ kaithem/src/staticfiles.py
@@ -1,12 +1,9 @@
 import os
-import time
 
-from starlette.middleware.gzip import GZipMiddleware
-from starlette.staticfiles import StaticFiles
-from starlette.types import Receive, Scope, Send
+import quart
 from structlog import get_logger
 
-from kaithem.api import web
+from kaithem.src import quart_app
 
 logger = get_logger(__name__)
 
@@ -14,64 +11,42 @@ sdn = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
 ddn = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "data")
 
 
-class TrivialCache:
-    def __init__(self, app):
-        self.app = app
-        self.cache = {}
-
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if scope["type"] == "http":
-            p = scope["path"]
-
-            old = self.cache.get(p, (None, 0))
-            if old[1] > time.time() - 3600:
-                for i in old[0]:
-                    await send(i)
-                return
-
-            c = []
-
-            async def send_wrapper(message):
-                c.append(message)
-                await send(message)
-
-            await self.app(scope, receive, send_wrapper)
-            self.cache[p] = (c, time.time())
-            try:
-                if len(self.cache) > 48:
-                    self.cache.pop(next(iter(self.cache)))
-            except Exception:
-                logger.exception("Error in TrivialCache")
-        else:
-            await self.app(scope, receive, send)
+@quart_app.app.route("/static/<path:path>")
+async def static_misc(path):
+    return await quart.send_file(os.path.join(ddn, "static", path))
 
 
-class RemoveOnePrefixMiddleware:
-    def __init__(self, app):
-        self.app = app
-
-    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
-        if "path" in scope:
-            if scope["path"].startswith("/"):
-                scope["path"] = scope["path"][1:]
-            scope["path"] = "/".join(scope["path"].split("/")[1:])
-        await self.app(scope, receive, send)
+@quart_app.app.route("/static/js/<path:path>")
+async def static_js(path):
+    return await quart.send_file(os.path.join(sdn, "js", path))
 
 
-def makeserver(dn):
-    return TrivialCache(GZipMiddleware(RemoveOnePrefixMiddleware(StaticFiles(directory=dn))))
+@quart_app.app.route("/static/css/<path:path>")
+async def static_css(path):
+    return await quart.send_file(os.path.join(sdn, "css", path))
 
 
-src = makeserver(sdn)
+@quart_app.app.route("/static/docs/<path:path>")
+async def static_docs(path):
+    return await quart.send_file(os.path.join(sdn, "docs", path))
 
 
-def add_apps():
-    web.add_asgi_app("/static/js/.*", src, "__guest__")
-    web.add_asgi_app("/static/css/.*", src, "__guest__")
-    web.add_asgi_app("/static/docs/.*", src, "__guest__")
-    web.add_asgi_app("/static/vue/.*", src, "__guest__")
-    web.add_asgi_app(
-        "/static/.*",
-        RemoveOnePrefixMiddleware(TrivialCache(GZipMiddleware(StaticFiles(directory=os.path.join(ddn, "static"))))),
-        "__guest__",
-    )
+## Catchall isn't working
+@quart_app.app.route("/static/mdicons/<path:path>")
+async def static_icons(path):
+    return await quart.send_file(os.path.join(ddn, "static", "mdicons", path))
+
+
+@quart_app.app.route("/static/freeboardsounds/<path:path>")
+async def static_fbsounds(path):
+    return await quart.send_file(os.path.join(ddn, "static", "freeboardsounds", path))
+
+
+@quart_app.app.route("/static/img/<path:path>")
+async def static_img(path):
+    return await quart.send_file(os.path.join(ddn, "static", "img", path))
+
+
+@quart_app.app.route("/static/fonts/<path:path>")
+async def static_fonts(path):
+    return await quart.send_file(os.path.join(ddn, "static", "fonts", path))
