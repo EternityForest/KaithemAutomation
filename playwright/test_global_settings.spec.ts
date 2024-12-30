054fdc5dff9e0fabd88b3eb8182322be43094827
Daniel Dunn <dannydunn@example.com>
2024-12-29 22:26:47 -0700

    Leftover references to persist

--- playwright/test_global_settings.spec.ts
+++ playwright/test_global_settings.spec.ts
@@ -5,17 +5,6 @@ test('test', async ({ page }) => {
     await login(page);
 
 
-    await page.getByRole('link', { name: '󰢻 Tools' }).click();
-    await page.getByRole('link', { name: '󰢻 System Settings' }).click();
-
-
-    await page.locator('input[name="url"]').click();
-    await page.locator('input[name="url"]').fill('example.com');
-    await page.locator('form').filter({ hasText: 'URL to redirect / to' }).getByRole('button').click();
-
-    await page.getByRole('link', { name: '󰢻 Tools' }).click();
-    await page.getByRole('link', { name: 'System Settings' }).click();
-    await expect(page.locator('input[name="url"]')).toHaveValue('example.com');
-   
+   // TODO this is now just a placeholder
     await logout(page);
 });
