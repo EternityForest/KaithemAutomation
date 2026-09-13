7b453217f436b0cc2ecbae1bf26e8841e282d7e4
Daniel Dunn <dannydunn@example.com>
2026-09-12 20:43:57 -0600

    extra api

--- kaithem/api/modules.py
+++ kaithem/api/modules.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from typing import Any
 
@@ -21,14 +22,16 @@ mutable_copy_resource = modules_state.mutable_copy_resource
 
 def set_resource_error(module: str, resource: str, error: str | None):
     """
-    Set an error notice for a resource.  Cleared when the resource is moved, updated or deleted,
+    Set an error notice for a resource.  Cleared when the resource is moved,
+    updated or deleted,
     or by setting the error to None.
     """
     modules_state.set_resource_error(module, resource, error)
 
 
 def filename_for_file_resource(module: str, resource: str) -> str:
-    """Given the module and resource for a file, return the actual file for a file resource, or
+    """Given the module and resource for a file, return
+     the actual file for a file resource, or
     file data dir for directory resource"""
 
     return modules_state.filename_for_file_resource(module, resource)
@@ -58,7 +61,8 @@ def scan_file_resources(module: str):
 
 @modules_state.modulesLock.required
 def get_resource_data(module: str, resource: str) -> dict[str, Any]:
-    "Get the dict data for a resource. May only be called under the modules_lock."
+    """Get the dict data for a resource.
+    May only be called under the modules_lock."""
     d = modules_state.ActiveModules[module][resource]
     return modules_state.mutable_copy_resource(d)
 
@@ -82,7 +86,8 @@ def insert_resource(module: str, resource: str, resourceData: ResourceDictType):
 
 @modules_lock.required
 def update_resource(module: str, resource: str, resourceData: ResourceDictType):
-    """Update an existing resource, triggering any relevant effects for that resource type.
+    """Update an existing resource, triggering any relevant
+     effects for that resource type.
     May only be called under the modules_lock."""
 
     if resource not in modules_state.ActiveModules[module]:
@@ -92,7 +97,7 @@ def update_resource(module: str, resource: str, resourceData: ResourceDictType):
         == resourceData["resource"]["type"]
     ):
         raise ValueError(
-            f"Resource {resource} in {module} is of type {resourceData['resource']['type']}"
+            f"Resource {resource} in {module} is of type {resourceData['resource']['type']}"  # noqa: E501
         )
     modules_state.raw_insert_resource(module, resource, resourceData)
     modules.handleResourceChange(module, resource)
@@ -100,7 +105,8 @@ def update_resource(module: str, resource: str, resourceData: ResourceDictType):
 
 @modules_lock.required
 def delete_resource(module: str, resource: str):
-    """Delete a resource, triggering any relevant effects for that resource type.
+    """Delete a resource, triggering
+    any relevant effects for that resource type.
     May only be called under the modules_lock.
     """
     modules.rmResource(module, resource)
@@ -111,6 +117,49 @@ def list_resources(module: str) -> list[str]:
     """List the resources in a module.
     May only be called under the modules_lock."""
     return list(modules_state.ActiveModules[module].keys())
+
+
+class FileResourceInfo:
+    def __init__(self, module: str, name: str, abs_path: str):
+        "Module providing the resource"
+        self.module = module
+        "Name as would be passed to resolve_file_resource"
+        self.name = name
+        "Absolute path to the file"
+        self.abs_path = abs_path
+
+        self.modified = os.path.getmtime(abs_path)
+        self.size = os.path.getsize(abs_path)
+
+
+@modules_lock.required
+def list_file_resources_by_prefix(prefix: str) -> list[FileResourceInfo]:
+    """List the file resources in all modules combined,
+    only including ones with a specified relative folder.
+
+    You could use this to list all fonts with public_resources/fonts/,
+
+    The return value is a list of FileResourceInfo objects
+    with module, name and absolute path.
+
+    """
+    combined: list[FileResourceInfo] = []
+    for i in sorted(list(modules_state.ActiveModules.keys())):
+        # FS operations can always fail with permission stuff
+        try:
+            path = modules_state.filename_for_file_resource(i, prefix)
+            if os.path.isdir(path):
+                for j in list(modules_state.ActiveModules[i].keys()):
+                    abs_path = modules_state.filename_for_file_resource(i, j)
+                    combined.append(FileResourceInfo(i, j, abs_path))
+                    if len(combined) > 1024 * 32:
+                        logging.warning(
+                            f"Too many file resources returned for {prefix}"
+                        )
+                        break
+        except Exception:
+            logging.exception("Error listing file resources")
+    return combined
 
 
 @modules_lock.required
@@ -128,6 +177,7 @@ def resolve_file_resource(relative_path: str) -> str | None:
 @modules_lock.required
 def save_resource(module: str, resource: str, resourceData: ResourceDictType):
     """Save a resource without triggering any other events.
-    Use this in your flush_unsaved handler. May only be called under the modules_lock.
+    Use this in your flush_unsaved handler.
+    May only be called under the modules_lock.
     """
     modules_state.raw_insert_resource(module, resource, resourceData)
